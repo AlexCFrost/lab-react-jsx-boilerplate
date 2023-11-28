1d87991f0cd7d5267bf7a2ad816c61e4c10e8446
AlexCFrost <siva.ganesh.c18@gmail.com>
2023-11-28 16:50:43 +0530

    Props

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,39 +1,41 @@
-import { Component } from "react";
+import React,{ Component } from "react";
 import "./App.css"
-import elephant from "./images/elephant.jpeg";
+import DataComponent from "./components/DataComponent";
+
+// import elephant from "./images/elephant.jpeg";
 
 export default class AppClass extends Component{
   
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
+  // imageData = ()=>{
+  //   let data = [
+  //     {
+  //       id:1,
+  //       img:elephant
+  //     },
+  //     {
+  //       id:2,
+  //       img:elephant
+  //     },
+  //     {
+  //       id:3,
+  //       img:elephant
+  //     },
+  //     {
+  //       id:4,
+  //       img:elephant
+  //     }
+  //   ]
+  //   return data;
+  // }
 
 
   render(){
 
-    const data = this.imageData();
+    const pic = DataComponent();
 
     return(
       <div id='grid'>
-      {data.map((el)=>{
+      {pic.map((el)=>{
         return ( <div key={el.id}>
           <img src={el.img}/>
         </div> )
